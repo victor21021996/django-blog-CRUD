ca36f3ddf650aad81d02da65c4df2b22890fde78
Ebube Okoli <ebube@Ebubes-MacBook-Pro.local>
2022-06-24 16:54:13 +0100

    Initial Commit

---
+++ I4G004466Y5X/blog/views.py
@@ -0,0 +1,33 @@
+from .models import Post
+from django.shortcuts import render
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView
+from django.views.generic.edit import UpdateView, DeleteView
+from django.urls import reverse_lazy
+
+
+class PostListView(ListView):
+    model = Post
+
+
+class PostCreateView(CreateView):
+    model = Post
+    fields = "__all__"
+    success_url = reverse_lazy("blog:all")
+
+
+class PostDetailView(DetailView):
+    model = Post
+
+
+class PostUpdateView(UpdateView):
+    model = Post
+    fields = "__all__"
+    success_url = reverse_lazy("blog:all")
+
+
+class PostDeleteView(DeleteView):
+    model = Post
+    fields = "__all__"
+    success_url = reverse_lazy("blog:all")
---
+++ README.md
@@ -0,0 +1,26 @@
+# Django Blog App
+
+A simple Blog App in Django.
+
+# Getting Started
+
+Install virtualenv `pip install virtualenv env`
+
+Create a virtual environment `python3 virtualenv env`
+
+Clone repository `git clone https://github.com/leonardokhorliey/django-blog`
+
+Enter virtual environment `source env/bin/activate `. This might be different on Windows OS.
+
+Enter Project directory `cd I4G004466Y5X`
+
+Create migrations `python manage.py makemigrations`
+
+Migrate `python manage.py migrate`. This should create a db.sqlite3 file in project directory.
+
+Start server `python manage.py runserver`
+
+
+# Make Requests
+
+Navigate to `http://127.0.0.1:8000/blog/` 
